36f7d86d1ad5f0be8790c00fa6374cc46a4251d4
victor <victorsilvaribeiro@hotmail.com>
2022-06-28 21:27:48 -0300

    Initial commit

---
+++ src/main/java/api/autentication/apiAutentication/entities/Autenticacao.java
@@ -0,0 +1,4 @@
+package api.autentication.apiAutentication.entities;
+
+public class Autenticacao {
+}
---
+++ src/test/java/api/autentication/apiAutentication/applicationTests.java
@@ -0,0 +1,13 @@
+package api.autentication.apiAutentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class applicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
